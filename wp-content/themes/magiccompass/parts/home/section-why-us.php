c96937312759f25ff0dbbb2ea333cc1a7e8d23d6
Synthetica <i.synthetica@gmail.com>
2019-05-20 18:26:42 +0300

    Why us section

--- wp-content/themes/magiccompass/parts/home/section-why-us.php
+++ wp-content/themes/magiccompass/parts/home/section-why-us.php
@@ -10,6 +10,8 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
+$why_us = get_field('why_us');
+
 $preferences = array(
     array(
         'icon' => 'fas fa-medal',
@@ -44,26 +46,32 @@ if (!empty($preferences)) {
     ?>
     <section id="why_us" class="ptb-20 ptb-md-40 ptb-lg-60">
         <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <h2 class="text-center mt-0 mb-20 mb-md-40 font-weight-600">Why us</h2>
+            <?php
+            if (!empty($why_us["section_title"])) {
+                ?>
+                <div class="row">
+                    <div class="col-12">
+                        <h2 class="text-center mt-0 mb-20 mb-md-40 font-weight-600"><?php echo $why_us["section_title"]; ?></h2>
+                    </div>
                 </div>
-            </div>
+                <?php
+            }
+            ?>
 
             <div class="row">
                 <?php
-                foreach ($preferences as $preference) {
+                foreach ($why_us["items"] as $item) {
                     ?>
                     <div class="col-12 col-lg-3 col-md-6 text-center sm-margin-30px-bottom wow fadeInUp" data-wow-delay="0.4s">
                         <div class="counter-feature-box-1 w-100 border-all p-5">
                             <div class="counter-box bg-white d-flex justify-content-center flex-column h-100">
-                                <i class="<?php echo $preference['icon']; ?> icon-extra-medium txt-gray-40-color mb-20"></i>
+                                <i class="<?php echo $item['icon']; ?> icon-extra-medium txt-gray-40-color mb-20"></i>
 
-                                <h6 class="d-block font-weight-600 txt-theme-color alt-font mb-0 timer" data-speed="2000" data-to="<?php echo $preference['count']; ?>">
-                                    <?php echo $preference['count']; ?>
+                                <h6 class="d-block font-weight-600 txt-theme-color alt-font mb-0 timer" data-speed="2000" data-to="<?php echo $item['counter']; ?>">
+                                    <?php echo $item['counter']; ?>
                                 </h6>
 
-                                <span><?php echo $preference['title']; ?></span>
+                                <span class="d-block prl-20"><?php echo $item['title']; ?></span>
                             </div>
                         </div>
                     </div>
