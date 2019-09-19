b59f2af3587a237a28e8dc0d6c8221b8696285c1
Synthetica <i.synthetica@gmail.com>
2019-09-19 12:09:11 +0300

    Show tabs

--- wp-content/themes/magiccompass/crm/parts/admin/moi-turisty.php
+++ wp-content/themes/magiccompass/crm/parts/admin/moi-turisty.php
@@ -14,19 +14,13 @@ $allowed_actions = array (
     'add_new', 'edit'
 );
 
+$active_tab = !empty($_GET['tab']) ? sanitize_text_field($_GET['tab']) : 'clients';
 ?>
-<ul>
-    <li><a href="?page=crm-moi-turisty&tab=clients">Clients</a></li>
-    <li><a href="?page=crm-moi-turisty&tab=clients_phones">Clients Phones</a></li>
-    <li><a href="?page=crm-moi-turisty&tab=claims">Claims</a></li>
-</ul>
-<?php
+<h2 class="nav-tab-wrapper">
+    <a href="?page=crm-moi-turisty&tab=clients" class="nav-tab<?php echo $active_tab == 'clients' ? ' nav-tab-active' : ''; ?>">Clients</a>
+    <a href="?page=crm-moi-turisty&tab=clients_phones" class="nav-tab<?php echo $active_tab == 'clients_phones' ? ' nav-tab-active' : ''; ?>">Clients Phones</a>
+    <a href="?page=crm-moi-turisty&tab=claims" class="nav-tab<?php echo $active_tab == 'claims' ? ' nav-tab-active' : ''; ?>">Claims</a>
+    <a href="?page=crm-moi-turisty&tab=claims_bookings" class="nav-tab<?php echo $active_tab == 'claims_bookings' ? ' nav-tab-active' : ''; ?>">Claim Bookings</a>
+</h2>
 
-if (
-    empty($_GET['tab']) || $_GET['tab'] === 'clients'
-) {
-    crm_show_template('admin/moi-turisty-clients.php');
-} else {
-    crm_show_template('admin/moi-turisty-'.$_GET['tab'].'.php');
-}
-?>
+<?php crm_show_template('admin/moi-turisty-'.$active_tab.'.php'); ?>
