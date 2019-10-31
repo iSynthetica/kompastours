70eec9638e99a8e50fa0641b922dacc223aa5e8a
Synthetica <i.synthetica@gmail.com>
2019-10-31 18:15:17 +0200

    Show tours on regions

--- wp-content/themes/magiccompass/parts/destination/region.php
+++ wp-content/themes/magiccompass/parts/destination/region.php
@@ -6,6 +6,9 @@
  * @subpackage Magiccompass/Parts/Destination
  * @version 0.0.9
  * @since 0.0.8
+ *
+ * @var $country_id
+ * @var $region_id
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit;
@@ -16,7 +19,7 @@ $template_args = array(
     'region' => $region_id,
     'tour_type'     => 1,
     'tour_kind'     => 1,
-    'meal_type'     => '560:512:498',
+    'meal_type'     => '560:512:498:496:388:1956',
     'items_per_page' => 12,
 );
 ?>
@@ -27,17 +30,7 @@ $template_args = array(
         ittour_show_template('form/section-search.php', array(
             'country'       => $country_id,
             'region'        => $region_id,
-//            'hotel'         => $hotel,
             'hotel_rating'  => '78:4',
-//            'from_city'     => $from_city,
-//            'date_from'     => $date_from,
-//            'date_till'     => $date_till,
-//            'night_from'    => $night_from,
-//            'night_till'    => $night_till,
-//            'adult_amount'  => $adult_amount,
-//            'child_amount'  => $child_amount,
-//            'child_age'     => $child_age,
-//            'price_limit'   => $price_limit,
             'tour_type'     => 1,
             'tour_kind'     => 1,
             'meal_type'     => '560:512:498',
